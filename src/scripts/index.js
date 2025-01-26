caf639e70fa9e54be8925068d38e4d25b848ab9a
Gabriel Lima <gabriellima1991@gmail.com>
2025-01-26 07:17:29 -0300

    Inicio domingo

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -24,3 +24,20 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
   });
+
+  const swiperSuggestion = new Swiper('.swiper-suggestions', {
+    loop: true,
+    effect: 'fade',
+    autoplay: {
+      delay: 7000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
